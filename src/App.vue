866ea636daa9fa6cf5b3920be026177b0212edb1
ww24kobe <1259481020@qq.com>
2020-08-20 15:23:24 +0800

    vue-webpack-cli

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    App根组件-{{ msg }}
+    <loginCom></loginCom>
+  </div>
+  
+</template>
+
+<script>
+import loginCom from "@/components/login.vue";
+export default {
+  data() {
+    return {
+      msg: "app-data",
+    };
+  },
+  components: {
+    loginCom,
+  },
+};
+</script>
+
+<style scoped>
+  div {
+    width: 500px;
+    height: 500px;
+    border: 2px solid #ccc;
+    text-align: center;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+// 导入Vue
+import Vue from "../node_modules/vue/dist/vue.js";
+
+import App from './App.vue';
+
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
+
+
+
